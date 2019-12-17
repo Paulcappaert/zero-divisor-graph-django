9b35b54cf4ee6105b933002ac91edb316387566c
Paul Cappaert <paul.cappaert@gmail.com>
2019-12-17 14:17:08 -0600

    user registration

--- django_starter_heroku/urls.py
+++ django_starter_heroku/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
     path('', include('home.urls')),
 ]
