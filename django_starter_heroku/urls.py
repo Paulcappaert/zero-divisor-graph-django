85693a380ca83445b9be2c8d82f533c3a526faca
Paul Cappaert <paul.cappaert@gmail.com>
2019-12-17 08:58:08 -0600

    added logout

--- django_starter_heroku/urls.py
+++ django_starter_heroku/urls.py
@@ -21,5 +21,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='logout'),
     path('', include('home.urls')),
 ]
