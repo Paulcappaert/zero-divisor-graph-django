8f3ea0d3376006d6851e72342c00337a811a404a
Paul Cappaert <paul.cappaert@gmail.com>
2020-01-24 19:28:36 -0500

    basic functionality

--- home/views.py
+++ home/views.py
@@ -1,4 +1,24 @@
 from django.shortcuts import render
+from zdg.zdg import ZeroDivisorGraph
 
 def home(request):
-    return render(request, 'home/home.html')
+    if(request.GET.get('g0', False)):
+        i = 1
+        edges = []
+        while(edge := request.GET.get('g' + str(i), False)):
+            a, b = map(int, edge.split('e'))
+            edges.append((a, b))
+            i += 1
+        graph = ZeroDivisorGraph(*edges)
+        semigroups = graph.semigroups()
+        if semigroups:
+            context = {
+                'semigroups': len(semigroups),
+            }
+        else:
+            context = {
+                'semigroups': '0',
+            }
+        return render(request, 'home/home.html', context)
+    else:
+        return render(request, 'home/home.html')
